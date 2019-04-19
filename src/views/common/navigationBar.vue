e6481fa92e06625f66d7f0d321cf2a7465595948
tangjun <tj_910601@163.com>
2019-04-19 16:26:57 +0800

    导航条样式

--- src/views/common/navigationBar.vue
+++ src/views/common/navigationBar.vue
@@ -6,15 +6,18 @@
     <div class="t-list">
       <el-row>
         <el-col :span="2">
-          <i class="t-bar fa fa-bars" @click="collapse"></i>
+          <i class="t-bar fa fa-bars" :class="[ isCollapse ? 't-rotate' : 't-norotate']" @click="collapse"></i>
         </el-col>
         <el-col :span="22" class="text-right">
-          <i class="t-icon el-icon-bell"></i>
+          <el-tooltip class="item" effect="dark" content="消息" placement="bottom">
+            <i class="t-icon el-icon-bell"></i>
+          </el-tooltip>
           <el-dropdown trigger="click" class="t-item">
             <img class="t-avatar" src="../../assets/images/avatar.jpg" alt>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>个人信息</el-dropdown-item>
-              <el-dropdown-item>退出</el-dropdown-item>
+            <span class="t-user">admin</span>
+            <el-dropdown-menu slot="dropdown" class="t-user-dropdown">
+              <el-dropdown-item><i class="t-user-icon fa fa-info-circle"></i>个人信息</el-dropdown-item>
+              <el-dropdown-item><i class="t-user-icon fa fa-sign-out"></i>退出</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </el-col>
@@ -32,11 +35,13 @@ export default {
   },
   methods:{
     collapse(){
-      this.$emit('changeCollapse', !this.isCollapse)
+      this.$emit('update:isCollapse', !this.isCollapse);
     }
   }
 }
 </script>
 <style lang="less" scoped>
 @import url("../../assets/less/common/navigationBar.less");
+</style>
+<style>
 </style>
